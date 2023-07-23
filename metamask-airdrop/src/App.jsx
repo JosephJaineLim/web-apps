84bbf5af3486aad1dfdbfd9655c91515c90601f9
Joseph Lim <limjosephjaine@gmail.com>
2023-07-23 23:30:30 +0800

    ...

--- metamask-airdrop/src/App.jsx
+++ metamask-airdrop/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div className='main'>
+      <Header />
       <Navbar />
     </div>
   )
---
+++ metamask-airdrop/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='app__header'>
+        <h1>Metamask Airdrop Guide</h1>
+    </div>
+  )
+}
+
+export default Header
